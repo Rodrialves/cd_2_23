f2c979726f8577b716990209ea92279dc7b5fbea
Rodrialves <rodrigornalves@gmail.com>
2023-05-03 18:17:07 +0100

    V3

--- noncanonical.c
+++ noncanonical.c
@@ -11,6 +11,21 @@
 #define FALSE 0
 #define TRUE 1
 
+/************** STATES ***************/
+#define START 0
+#define FLAG_RCV 1
+#define A_RCV 2
+#define C_RCV 3
+#define BCC_OK 4
+#define STOP 5
+
+
+/************** FLAGS ***************/
+#define FLAG 0x5c
+#define A 0x01
+#define C_SET 0x03
+#define C_UA 0x07 
+
 volatile int STOP=FALSE;
 
 int main(int argc, char** argv)
@@ -78,9 +93,67 @@ int main(int argc, char** argv)
         printer[i]=aux[0];
         if (aux[0]=='\0') STOP=TRUE;
         i++;
+
+        // switch (state){
+
+        //     case START:
+        //         if(aux == FLAG){
+        //             state = FLAG_RCV;
+        //         }
+        //         break;
+            
+        //     case FLAG_RCV:
+        //         if(aux == A){
+        //             state = A_RCV;
+        //         }
+        //         if(aux == FLAG_RCV){
+        //             break;
+        //         }
+        //         else{
+        //             state = START;
+        //         }
+        //         break;
+
+        //     case A_RCV:
+        //         if(aux == FLAG){
+        //             state = FLAG_RCV;
+        //         }
+        //         if(aux = C_SET){
+        //             state = C_RCV;
+        //         }
+        //         else{
+        //             state = START;
+        //         }
+        //         break;
+        //     case C_RCV:
+        //         unsigned char x = A^C_SET;
+        //         if(aux == FLAG){
+        //             state = FLAG_RCV;
+        //         }
+        //         if(aux == x){
+        //             state = BCC_OK;
+        //         }
+        //         else{
+        //             state = START;
+        //         }
+        //         break;
+        //     case BCC_OK:
+        //         if(aux == FLAG){
+        //             state = STOP; /* Chegar aqui é bom */
+        //         }
+        //         else{
+        //             state = START;
+        //         }
+        //         break;
+        //     }
+        // }
     }
         
-        printf("\n%s\n",printer);
+        // printf("\n%s\n",printer);
+
+        for (int i=0;i<5;i++){
+            printf("\n%x\n",printer[i]);
+        }
 
 
 
